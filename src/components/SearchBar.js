8444ac5ecd581c8906929c4525281a11f443a325
dippsyboy <ndipp999@gmail.com>
2023-11-02 17:59:36 +0100

    Adding of State to the SearchBar, and mock functionallity for each button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/SearchBar.css'; // Import the CSS file
 
 function SearchBar() {
-    return (
-      <div className="SearchBar">
-        <form>
-          <div className="button-group">
-            <button>Best Match</button>
-            <button>Highest Rated</button>
-            <button>Most Reviewed</button>
-          </div>
-          <div className="search-bars">
-            <input placeholder="Search city" />
-            <input placeholder="Search type of food" />
-          </div>
-          <div className="button-search">
-            <button type="submit">Search</button>
-          </div>
-        </form>
-      </div>
-    );
+  const [search, setSearch] = useState('');
+  const [location, setLocation] = useState('');
+  const [sorting, setSorting] = useState('BestMatch');
+
+  function handleClick(event) {
+    setSorting(event.target.value);
+  };
+
+  function handleChange(event){
+    event.preventDefault();
+    const {name, value} = event.target
+    if (name === 'location'){
+      setLocation(value);
+    }else if (name === 'search'){
+      setSearch(value);
+    }
+  };
+
+  function handleSubmit(event){
+    alert(`Searching Yelp for ${search} in ${location}, sort by:${sorting}`)
+  }
+  return (
+    <div className="SearchBar">
+        <div className="button-group">
+          <button value='BestMatch' onClick={handleClick} className={sorting === 'BestMatch' ? 'active' : ''} >Best Match</button>
+          <button value='HighestRated'onClick={handleClick} className={sorting === 'HighestRated' ? 'active' : ''} >Highest Rated</button>
+          <button  value='MostReviewed' onClick={handleClick} className={sorting === 'MostReviewed' ? 'active' : ''} >Most Reviewed</button>
+        </div>
+      <form>
+        <div className="search-bars">
+          <input 
+          type='text' 
+          name='location' 
+          value={location}
+          placeholder="Search city" 
+          onChange={handleChange} />
+          <input 
+          type='text' 
+          name='search'
+          value={search} 
+          placeholder="Search type of food" 
+          onChange={handleChange} />
+        </div>
+        <div className="button-search">
+          <button type="submit" onClick={handleSubmit}>Search</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
